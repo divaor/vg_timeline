fbb31b1f723b6e2ca56767c5a5e562eb339e8cb5
Diego Valencia <divaor@gmail.com>
2010-06-09 01:04:46 -0600

    Added clear text in search fields

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -495,6 +495,12 @@ function showHide(element) {
   }
 }
 
+function clearText(textField, element) {
+  $(textField).value = '';
+  $(textField).focus();
+  element.hide();
+}
+
 function searchText(element, type, text) {
   if (type == 0) {
     if (element.value == text) {
@@ -512,6 +518,15 @@ function searchText(element, type, text) {
       });
       element.value = text;
     }
+  }
+}
+
+function showClear(clear, element) {
+  if (element.value != '') {
+    $(clear).show();
+  }
+  else {
+    $(clear).hide();
   }
 }
 
